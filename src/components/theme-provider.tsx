0269b36ba205ae61b538c59b41bc9747a76b06a5
feder1402 <feder1402@users.noreply.github.com>
2025-09-26 15:38:29 -0700

    Migrated to vite in place

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,5 @@
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemeProviderProps } from "next-themes"
+
+export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
